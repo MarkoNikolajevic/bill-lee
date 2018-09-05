6b8a13e273ad0d92b8f58bc232c1a609c978676f
Marko Nikolajevic <mrknikolajevic@gmail.com>
2018-09-05 12:35:58 +0200

    Code navbar mobile animation functions

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,4 @@
+// Animated caret down
 const caret = document.querySelector('.caret-down');
 const header = document.querySelector('.main-header');
 
@@ -7,3 +8,30 @@ header.addEventListener('mouseover', function() {
 		caret.classList.remove('translate');
 	});
 });
+
+// Mobile navbar
+mobileNav();
+
+function mobileNav() {
+	const hamburger = document.querySelector('.hamburger-menu');
+	const nav = document.querySelector('.nav-mobile');
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('animated');
+		nav.classList.toggle('slide-right');
+		nav.classList.toggle('display-none');
+		closeNav();
+	});
+}
+
+function closeNav() {
+	let links = document.querySelectorAll('.mobile-nav-link');
+	links = Array.from(links);
+	links.forEach(function(elem) {
+		elem.addEventListener('click', function() {
+			const hamburger = document.querySelector('.hamburger');
+			const nav = document.querySelector('.nav-mobile');
+			nav.classList.add('display-none');
+			hamburger.classList.remove('animated');
+		});
+	});
+}
